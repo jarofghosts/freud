f210805a0bc310427f6d1ae32fba1ce4753885dc
jarofghosts <me@jessekeane.me>
2013-03-25 19:07:36 -0700

    fleshing out rendering

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var fs = require('fs'),
+  md = require('node-markdown').Markdown,
   sourceDirectory = '/home/jarofghosts',
   targetDirectory = '/home/jarofghosts/flog';
 
+var renderFile = function (data, filename) {
+  fs.writeFile(filename, md(data), function (err) {
+    if (err) { throw err; }
+    console.log(filename + ' rendered.');
+  });
+};
+
+var parseMarkdown = function (filename) {
+  fs.readFile(sourceDirectory + '/' + filename, { encoding: 'utf-8' }, function (err, data) {
+    if (err) { throw err; }
+    var outputFile = filename.replace(/\.md/, '.html');
+    renderFile(data, outputFile);
+  });
+};
+
 fs.watch(sourceDirectory, { persistent: true }, function (event, filename) {
   if (!filename.match(/^\./) && filename.match(/\.md$/)) {
-    console.log('event = ' + event + ', filename = ' + filename);
+    parseMarkdown(filename);
   }
 });
