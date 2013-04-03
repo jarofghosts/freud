e4e00fee914490f5bbc70713ce83671256570bb9
jarofghosts <me@jessekeane.me>
2013-04-02 18:52:43 -0700

    breaking out config items to json file

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 var fs = require('fs'),
-  md = require('node-markdown').Markdown,
-  sourceDirectory = '/home/jarofghosts',
-  targetDirectory = '/home/jarofghosts/flog';
+  freud = require('./lib/freud.js');
 
-var renderFile = function (data, filename) {
-  fs.writeFile(targetDirectory + '/' + filename, md(data), function (err) {
-    if (err) { throw err; }
-    console.log(filename + ' rendered.');
-  });
-};
-
-var parseMarkdown = function (filename) {
-  fs.readFile(sourceDirectory + '/' + filename, { encoding: 'utf8' }, function (err, data) {
-    if (err) { throw err; }
-    var outputFile = filename.replace(/\.md/, '.html');
-    renderFile(data, outputFile);
-  });
-};
-
-fs.watch(sourceDirectory, { persistent: true }, function (event, filename) {
+fs.watch(freud.sourceDirectory, { persistent: true }, function (event, filename) {
   if (!filename.match(/^\./) && filename.match(/\.md$/)) {
-    parseMarkdown(filename);
+    freud.parseMarkdown(filename, function (outputFile, data) {
+      freud.renderFile(outputFile, data);
+    });
   }
 });
