14ad0052d773540a6115cfdb5b473c0a2caddaa7
jarofghosts <me@jessekeane.me>
2013-04-02 20:37:31 -0700

    expose option for destructive file changes

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var fs = require('fs'),
-  freud = require('./lib/freud.js');
+  freud = require('./lib/freud.js'),
+  parsingStack = [];
 
-fs.watch(freud.sourceDirectory, { persistent: true }, function (event, filename) {
-  if (!filename.match(/^\./) && filename.match(/\.md$/)) {
-    freud.parseMarkdown(filename, function (outputFile, data) {
-      freud.renderFile(outputFile, data);
-    });
+fs.exists(freud.sourceDirectory, function (exists) {
+
+  if (!exists) {
+    console.log('source `' + freud.sourceDirectory + '` does not exist.');
+    return -1;
   }
+
+  fs.watch(freud.sourceDirectory, { persistent: true }, function (event, filename) {
+    if (!filename.match(/^\./) && filename.match(/\.md$/) && parsingStack.indexOf(filename) === -1) {
+      parsingStack.push(filename);
+      fs.exists(freud.sourceDirectory + filename, function (inputFileExists) {
+        if (inputFileExists) {
+          freud.parseMarkdown(filename, function (outputFile, data) {
+            freud.renderFile(outputFile, data, function () {
+              parsingStack.splice(parsingStack.indexOf(filename), 1);
+            });
+          });
+        } else {
+          if (freud.destructive) {
+            freud.unlink(filename, function (oldFile) {
+              console.log('unlinked ' + oldFile);
+              parsingStack.splice(parsingStack.indexOf(filename), 1);
+            });
+          }
+        }
+      });
+    }
+  });
+
 });
