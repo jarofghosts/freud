8ec792ad78673fa7ab9afaac9c9bcebace26e887
jarofghosts <me@jessekeane.me>
2013-04-03 19:35:07 -0700

    more config options, skip initial dir check

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ var fs = require('fs'),
   freud = require('./lib/freud.js'),
   parsingStack = [];
 
-fs.exists(freud.sourceDirectory, function (exists) {
-
-  if (!exists) {
-    console.log('source `' + freud.sourceDirectory + '` does not exist.');
-    return -1;
-  }
-
-  fs.watch(freud.sourceDirectory, { persistent: true }, function (event, filename) {
-    if (!filename.match(/^\./) && filename.match(/\.md$/) && parsingStack.indexOf(filename) === -1) {
-      parsingStack.push(filename);
-      fs.exists(freud.sourceDirectory + filename, function (inputFileExists) {
-        if (inputFileExists) {
+fs.watch(freud.sourceDirectory, { persistent: true }, function (event, filename) {
+  if ((freud.ignoreDot && !filename.match(/^\./)) && parsingStack.indexOf(filename) === -1) {
+    parsingStack.push(filename);
+    fs.exists(freud.sourceDirectory + filename, function (inputFileExists) {
+      if (inputFileExists) {
+        if (filename.match(/\.md$/)) {
           freud.parseMarkdown(filename, function (outputFile, data) {
             freud.renderFile(outputFile, data, function () {
               parsingStack.splice(parsingStack.indexOf(filename), 1);
             });
           });
-        } else {
-          if (freud.destructive) {
-            freud.unlink(filename, function (oldFile) {
-              console.log('unlinked ' + oldFile);
+        } else if (filename.match(/\.jade$/)) {
+          freud.parseJade(filename, function (outputFile, data) {
+            freud.renderFile(outputFile, data, function () {
               parsingStack.splice(parsingStack.indexOf(filename), 1);
             });
-          }
+          });
         }
-      });
-    }
-  });
 
+      } else {
+        if (freud.destructive) {
+          freud.unlink(filename, function (oldFile) {
+            console.log('unlinked ' + oldFile);
+            parsingStack.splice(parsingStack.indexOf(filename), 1);
+          });
+        }
+      }
+    });
+  }
 });
