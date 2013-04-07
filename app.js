9ae17294024ef2b4b10622cac1d09a51cb29e4d7
jarofghosts <me@jessekeane.me>
2013-04-06 18:41:00 -0700

    making freud a real module

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var fs = require('fs'),
-  Freud = require('./lib/freud.js'),
+  Freud = require('./lib/freud.js').Freud,
   freud = new Freud('/home/jarofghosts/flog-src', '/home/jarofghosts/flog');
+
+freud.listen('txt', function (file) {
+  file.data = 'hey!';
+  console.log('raaan');
+  return file;
+});
 
 freud.go();
