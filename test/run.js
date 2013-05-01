05c0ce550f62723eee3969d18292bbc904e01cb1
jarofghosts <me@jessekeane.me>
2013-04-30 20:24:38 -0700

    upped version

--- test/run.js
+++ test/run.js
@@ -27,7 +27,7 @@ freud.on('compiling', function (filename) {
 });
 
 freud.on('started', function (version) {
-  assert.equal(version.version, '0.1.5');
+  assert.equal(version.version, '0.1.6');
 });
 
 freud.on('compiled', function (filename) {
